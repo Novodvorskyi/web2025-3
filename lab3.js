b0293c660e4ac596768b53958fbcd94017b4906b
Роман Новодворський web2025-3 <novodvorskyiroma@gmail.com>
2025-03-11 09:14:29 +0200

    Виправив помилки

--- lab3.js
+++ lab3.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 const program = new Command();
 
 program
-    .requiredOption('-i, --input <path>', 'шлях до файлу для читання')
+    .option('-i, --input <path>', 'шлях до файлу для читання')
     .option('-o, --output <path>', 'шлях до файлу для запису')
     .option('-d, --display', 'вивести результат у консоль');
 
 program.parse(process.argv);
 const options = program.opts();
+
+if (!options.input) {
+    console.error('Please, specify input file');
+    process.exit(1);
+}
 
 if (!fs.existsSync(options.input)) {
     console.error('Cannot find input file');
